6d6b7dece18525926505e3ef498b290a544030d6
Ahmed Aboelyazeed <ahmdaeyz@megatrust.co>
2021-12-28 16:37:30 +0200

    Add RTL support

--- lib/src/lines_widget.dart
+++ lib/src/lines_widget.dart
@@ -4,7 +4,7 @@ import 'lines_painter.dart';
 import 'tree_node.dart';
 import 'tree_node_scope.dart';
 import 'tree_view_theme.dart';
-
+import 'dart:math' as math;
 /// Widget responsible for indenting nodes and drawing lines (if enabled).
 class LinesWidget extends StatelessWidget {
   /// Creates a [LinesWidget].
@@ -13,7 +13,7 @@ class LinesWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final treeNodeScope = TreeNodeScope.of(context);
-
+    final isRTL = Directionality.of(context) == TextDirection.rtl;
     late final child = SizedBox(
       width: treeNodeScope.indentation,
       height: double.infinity,
@@ -21,22 +21,32 @@ class LinesWidget extends StatelessWidget {
 
     switch (treeNodeScope.theme.lineStyle) {
       case LineStyle.scoped:
-        return CustomPaint(
-          painter: LinesPainter(
-            linesToBeDrawn: treeNodeScope.node.scopedLines,
-            theme: treeNodeScope.theme,
-          ),
-          child: child,
-        );
+        return Transform(
+            alignment: Alignment.center,
+            transform: isRTL
+                ? (Matrix4.identity()..rotateY(-math.pi))
+                : Matrix4.identity(),
+            child: CustomPaint(
+              painter: LinesPainter(
+                linesToBeDrawn: treeNodeScope.node.scopedLines,
+                theme: treeNodeScope.theme,
+              ),
+              child: child,
+            ));
 
       case LineStyle.connected:
-        return CustomPaint(
-          painter: LinesPainter(
-            linesToBeDrawn: treeNodeScope.node.connectedLines,
-            theme: treeNodeScope.theme,
-          ),
-          child: child,
-        );
+        return Transform(
+            alignment: Alignment.center,
+            transform: isRTL
+                ? (Matrix4.identity()..rotateY(-math.pi))
+                : Matrix4.identity(),
+            child: CustomPaint(
+              painter: LinesPainter(
+                linesToBeDrawn: treeNodeScope.node.connectedLines,
+                theme: treeNodeScope.theme,
+              ),
+              child: child,
+            ));
 
       case LineStyle.disabled:
       default:
